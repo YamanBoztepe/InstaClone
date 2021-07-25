c597a8ffd9b525a3ee88ed0c8c5682320059a140
Yaman Boztepe <yaman.boztepe@gmail.com>
2021-07-25 20:53:49 +0300

    DeepLinks and Unit Tests are added

---
+++ InstaClone/Module/SignUp/Model/SignUpFormModel.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpFormModel.swift
+//  InstaClone
+//
+//  Created by Yaman Boztepe on 23.07.2021.
+//
+
+import Foundation
+
+struct SignUpFormModel {
+    let firstName: String?
+    let lastName: String?
+    let email: String?
+    let password: String?
+    let repeatPassword: String?
+    let address: Address?
+}
+
+struct Address {
+    let country: String?
+    let city: String?
+}
